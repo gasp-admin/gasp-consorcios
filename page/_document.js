0e12054d18c9d7e9aea35dfa3013f0faf4227458
gasp-admin <javiergp@live.com.ar>
2026-05-10 12:44:46 -0300

    Create _document.js

---
+++ page/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#080D1A" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
