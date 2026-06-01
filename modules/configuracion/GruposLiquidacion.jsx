32f80d5f542fa2a2c2ecb8da19cc0068a4542efc
gasp-admin <javiergp@live.com.ar>
2026-06-01 08:09:18 -0300

    fix: GruposLiquidacion inject sin redecl vars+setters

--- modules/configuracion/GruposLiquidacion.jsx
+++ modules/configuracion/GruposLiquidacion.jsx
@@ -9,7 +9,7 @@ import { getCuentaCorriente, siroProxy, enviarLiquidacion, gestionarClienteGASP,
 import { Btn, BtnSec, Card, Input, Sel, Badge, Msg, BarraListado } from '../../components/ui'
 
 export default function GruposLiquidacion() {
-  const { session, consorcioActivo, setConsorcioActivo, unidades, setUnidades, copropietarios, setCopropietarios, expensas, setExpensas, proveedores, setProveedores, adminPerfil, setAdminPerfil, cargando, esSuperAdmin, consorcios, setConsorcios, pagina, setPagina, menuAbierto, setMenuAbierto, isMobile, navItems, secciones, navActivo, formCon, setFormCon, msgCon, cargarConsorcio, cargarConsorcios, guardarConsorcio } = useApp()
+  const { session, cargando, esSuperAdmin, consorcios, setConsorcios, consorcioActivo, setConsorcioActivo, unidades, setUnidades, copropietarios, setCopropietarios, expensas, setExpensas, proveedores, setProveedores, adminPerfil, setAdminPerfil, formCon, setFormCon, msgCon, cargarConsorcio, cargarConsorcios, guardarConsorcio, pagina, setPagina, menuAbierto, setMenuAbierto, isMobile, navItems, secciones, navActivo } = useApp()
   const uid = session?.user?.id
   const consorcioId = consorcioActivo?.id
   const [grupos, setGrupos]     = useState([])
