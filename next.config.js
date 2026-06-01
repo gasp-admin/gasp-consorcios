b5a1bd436e197e1fe01e3b83dd1388589e4ace23
gasp-admin <javiergp@live.com.ar>
2026-06-01 13:19:12 -0300

    fix: ImportarPDF mensaje+expensa existente — 1780330752

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780330422',
+  generateBuildId: async () => 'build-1780330752',
 }
 module.exports = nextConfig
