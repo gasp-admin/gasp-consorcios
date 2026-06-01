60110450268f9a79b75aa4551f4279cacb515551
gasp-admin <javiergp@live.com.ar>
2026-06-01 07:53:01 -0300

    fix: 12 módulos rebuild V59 limpio — DEPLOY 1780311181

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780310927',
+  generateBuildId: async () => 'build-1780311181',
 }
 module.exports = nextConfig
