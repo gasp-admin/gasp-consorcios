6cf8493b4864292407ee1f7ea5b71c074aabd383
gasp-admin <javiergp@live.com.ar>
2026-06-01 07:39:59 -0300

    fix: pLabel duplicados eliminados + deploy 1780310399

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780310303',
+  generateBuildId: async () => 'build-1780310399',
 }
 module.exports = nextConfig
