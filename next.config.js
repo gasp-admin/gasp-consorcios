63751d3d2a23acd63c60eb169c663dfb444bc571
gasp-admin <javiergp@live.com.ar>
2026-06-01 07:44:45 -0300

    fix: EnviarNotificacion + Cobranzas + HistorialLiq catch fix — 1780310685

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780310399',
+  generateBuildId: async () => 'build-1780310685',
 }
 module.exports = nextConfig
