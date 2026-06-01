2229c17c21617b3e8b6aa32ec16d5abe0cd1f721
gasp-admin <javiergp@live.com.ar>
2026-06-01 08:06:51 -0300

    fix: inject inteligente sin redeclaraciones — 1780312011

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780311854',
+  generateBuildId: async () => 'build-1780312011',
 }
 module.exports = nextConfig
