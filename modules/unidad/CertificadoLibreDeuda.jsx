deb1a6596c432d5213d1e46f1c23dc27deac4433
gasp-admin <javiergp@live.com.ar>
2026-06-01 08:04:13 -0300

    fix: CertificadoLibreDeuda limpio V59 inject fijo

--- modules/unidad/CertificadoLibreDeuda.jsx
+++ modules/unidad/CertificadoLibreDeuda.jsx
@@ -5,12 +5,13 @@ import { SUPA_URL, AZ, AZ2, VD, RJ, AM, GR, BG, SUPERADMIN } from '../../lib/con
 import { fmt, fmtD, fmtN, periodoLabel, periodoActual, nextId, colGasto } from '../../lib/formatters'
 import { exportarExcel } from '../../lib/exportExcel'
 import { exportarPDF, generarPDFLiquidacion } from '../../lib/exportPdf'
-import { getCuentaCorriente, siroProxy, enviarLiquidacion, generarCertificadoLibreDeuda, gestionarClienteGASP, crearDemoConsorcios } from '../../api/edgeFunctions'
+import { getCuentaCorriente, siroProxy, enviarLiquidacion, gestionarClienteGASP, crearDemoConsorcios } from '../../api/edgeFunctions'
 import { Btn, BtnSec, Card, Input, Sel, Badge, Msg, BarraListado } from '../../components/ui'
 
 export default function CertificadoLibreDeuda() {
-  const { session, consorcioActivo, unidades, copropietarios, expensas, adminPerfil } = useApp()
-  const uid = session?.user?.id session, consorcioId, consorcioActivo, unidades, copropietarios, expensas }
+  const { session, consorcioActivo, unidades, copropietarios, expensas, proveedores, adminPerfil } = useApp()
+  const uid = session?.user?.id
+  const consorcioId = consorcioActivo?.id
   const [tab, setTab]             = useState('nuevo')
   const [certificados, setCertificados] = useState([])
 
