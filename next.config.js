1c41e30d7566875019fa5657b0f186abe9bbd0ed
gasp-admin <javiergp@live.com.ar>
2026-06-01 07:24:24 -0300

    fix: restaurar pLabel en 7 módulos + build 1780309463

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780278423',
+  generateBuildId: async () => 'build-1780309463',
 }
 module.exports = nextConfig
