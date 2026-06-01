631143fd8c013ee2ef2c87f5531e894a030e5341
gasp-admin <javiergp@live.com.ar>
2026-06-01 07:38:23 -0300

    fix: pLabel todos los modulos — deploy final 1780310303

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780309733',
+  generateBuildId: async () => 'build-1780310303',
 }
 module.exports = nextConfig
