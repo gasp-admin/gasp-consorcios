be1517ec8bb3760843b65781929015bee49b6acd
gasp-admin <javiergp@live.com.ar>
2026-06-01 18:35:52 -0300

    fix: EstadoFinanciero ingresos historicos — 1780349752

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780343570',
+  generateBuildId: async () => 'build-1780349752',
 }
 module.exports = nextConfig
