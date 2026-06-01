f90373e0e41d307b30dc72854ddd52df90ec3303
gasp-admin <javiergp@live.com.ar>
2026-06-01 16:44:40 -0300

    fix: ImportarPDF credito+debito separados cta cte — 1780343079

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780342480',
+  generateBuildId: async () => 'build-1780343079',
 }
 module.exports = nextConfig
