76c10a359b8a927b888d77414b18f0583c24ab0b
gasp-admin <javiergp@live.com.ar>
2026-06-01 13:26:00 -0300

    fix: ImportarPDF con_liquidacion_uf + Dashboard btn — 1780331159

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  generateBuildId: async () => 'build-1780330752',
+  generateBuildId: async () => 'build-1780331159',
 }
 module.exports = nextConfig
